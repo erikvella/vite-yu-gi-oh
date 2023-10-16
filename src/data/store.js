f5b206cd85f8fd147fb67c53a571432de60bf40e
erikvella <vella.erik1@gmail.com>
2023-10-16 11:07:54 +0100

    [fix] getApi

--- src/data/store.js
+++ src/data/store.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue';
 export const store = reactive({
   apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
   cardsList: [],
-  archetypeToSearch:'Alien',
+  archetypeToSearch:null,
   isLoading : true,
   archetypeList : ['Alien' , 'Noble Knight' , 'Melodious']
 })
